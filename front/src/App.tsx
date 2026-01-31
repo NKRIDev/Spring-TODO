ff6388dc103b3f30fb061bdac888853e008f89f4
Nkri97133 <nkri.dev@gmail.com>
2026-01-31 17:33:57 +0100

    feat : new front with React

---
+++ front/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen w-full">
+        Hello World !
+      </div>
+    </>
+  )
+}
+
+export default App
